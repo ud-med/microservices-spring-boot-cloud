b762909e106cdb3f7f4c10c94bd220ec90123f39
mohamed <med2bouanane@gmail.com>
2020-04-11 20:34:50 +0200

    create maven project

---
+++ microservices-spring-boot-cloud/src/main/java/com/ud/microservice/limitservice/MicroservicesSpringBootCloudApplication.java
@@ -0,0 +1,13 @@
+package com.ud.microservice.limitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringBootCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSpringBootCloudApplication.class, args);
+    }
+
+}
---
+++ microservices-spring-boot-cloud/src/test/java/com/ud/microservice/limitservice/MicroservicesSpringBootCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ud.microservice.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSpringBootCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
